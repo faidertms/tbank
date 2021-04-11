1ef1a1590fc4255ab4cbf9c876c0cc5d8a26e5c2
Thiago Sales <thiagomsales@hotmail.com>
2021-04-11 18:51:29 -0300

    fix: remove contas seed and add it inside usuario

--- backend/src/database/seeds/01-usuarios.js
+++ backend/src/database/seeds/01-usuarios.js
@@ -1,7 +1,9 @@
-
 exports.seed = async function (knex) {
+  await knex('transacoes').del();
+  await knex('favorecidos').del();
   await knex('contas').del();
   await knex('usuarios').del();
+
   await knex('usuarios').insert([
     {
       id: 1,
@@ -10,4 +12,14 @@ exports.seed = async function (knex) {
       telefone_celular: 85988888888
     },
   ]);
+  await knex('contas').insert([
+    {
+      numero_identificador: 1,
+      saldo: 1000,
+      limite: 500,
+      usuario_id: 1,
+    },
+  ]);
+
+  await knex.raw('ALTER SEQUENCE usuarios_id_seq RESTART WITH 2');
 };
